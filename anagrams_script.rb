8dbf2434b0ec02a09345d7f37d81df31bc052cf8
roger yee <ryee926@gmail.com>
2018-02-09 11:25:07 -0800

    fixes bug where if first letter case was ignored

--- anagrams_script.rb
+++ anagrams_script.rb
@@ -9,19 +9,19 @@ class Check_anagrams
 
   def rearrange(word)
     letters = word.split("")
-    sorted = letters.sort
-    conform = sorted.map(&:downcase)
+    conform = letters.map(&:downcase)
+    sorted = conform.sort
   end
 
   def are_anagrams(word_1, word_2)
     sorted_1 = rearrange(word_1)
     sorted_2 = rearrange(word_2)
-
-    if (((sorted_1 & ['a', 'e', 'i', 'o', 'u', 'y']).any? == false) && (sorted_2 & ['a', 'e', 'i', 'o', 'u', 'y']).any? == false)
+    vowels = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U', 'y', 'Y']
+    if (((sorted_1 & vowels).any? == false) && (sorted_2 & vowels).any? == false)
       "Both words are not real words."
-    elsif ((sorted_1 & ['a', 'e', 'i', 'o', 'u', 'y']).any? == false)
+    elsif ((sorted_1 & vowels).any? == false)
       "The first word is not a real word."
-    elsif ((sorted_2 & ['a', 'e', 'i', 'o', 'u', 'y']).any? == false)
+    elsif ((sorted_2 & vowels).any? == false)
       "The second word is not a real word."
     elsif (sorted_1 == sorted_2)
       "These words are anagrams."
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -6,7 +6,7 @@ describe('#anagrams') do
   it("verifies whether or not the two words are anagrams") do
     expect(Check_anagrams.new("dog", "god").are_anagrams("dog", "god")).to(eq("These words are anagrams."))
   end
-  
+
   it("accounts for words that have varying capitalizations") do
     expect(Check_anagrams.new("Dog", "God").are_anagrams("Dog", "God")).to(eq("These words are anagrams."))
   end
@@ -22,5 +22,7 @@ describe('#anagrams') do
   it("checks if either word is real") do
     expect(Check_anagrams.new("dgd", "gdg").are_anagrams("dgd", "gdg")).to(eq("Both words are not real words."))
     end
+
+
   end
 end
