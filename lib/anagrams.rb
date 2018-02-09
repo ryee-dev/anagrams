0ad22363ae2839ff062f0eed0d964f4ec137d74f
roger yee <ryee926@gmail.com>
2018-02-09 10:55:53 -0800

    previous commit actually passes future test, this commit passes 3rd test

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -7,19 +7,22 @@ class Check_anagrams
 
   def rearrange(word)
     letters = word.split("")
-    if (letters.include?('a', 'e', 'i', 'o', 'u', 'y') == false)
-      "Not a real word."
-    end
     sorted = letters.sort
     conform = sorted.map(&:downcase)
   end
 
   def are_anagrams(word_1, word_2)
-    word_1 = "dog"
+    word_1 = "dgd"
     word_2 = "god"
     sorted_1 = rearrange(word_1)
     sorted_2 = rearrange(word_2)
-    if (sorted_1 == sorted_2)
+    # if (((sorted_1 & ['a', 'e', 'i', 'o', 'u', 'y']).any? == false) && (sorted_2 & ['a', 'e', 'i', 'o', 'u', 'y']).any? == false)
+    #   "Both words are not real words."
+    if ((sorted_1 & ['a', 'e', 'i', 'o', 'u', 'y']).any? == false)
+      "The first word is not a real word."
+    # elsif ((sorted_2 & ['a', 'e', 'i', 'o', 'u', 'y']).any? == false)
+    #   "The second word is not a real word."
+    elsif (sorted_1 == sorted_2)
       "These words are anagrams."
     else
       "These words are NOT anagrams."
